3b5a598a7cae0bbfc2c225f8deff0f5d54bcac4a
antonhorobets <shxbb@s-mxs.net>
2024-09-30 16:58:01 +0200

    init

---
+++ src/test/java/sk/stuba/pks/PksApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stuba.pks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
