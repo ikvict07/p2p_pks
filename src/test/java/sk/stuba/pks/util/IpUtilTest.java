764691f0bcacd9d33763e36c0e5ab37c45612046
ikvict07 <antongorobec101@gmail.com>
2024-10-06 22:57:08 +0200

    Get ip test

---
+++ src/test/java/sk/stuba/pks/util/IpUtilTest.java
@@ -0,0 +1,15 @@
+package sk.stuba.pks.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IpUtilTest {
+
+    @Test
+    void getIp() {
+        System.out.println(IpUtil.getIp());
+    }
+
+
+}
