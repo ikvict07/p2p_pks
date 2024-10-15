6e7367deaeca782b36c9c516fdfe3f938340dcb0
ikvict07 <antongorobec101@gmail.com>
2024-10-15 23:08:21 +0200

    Fixed injection

--- src/main/java/sk/stuba/pks/starter/InjectMessageListenersAnnotationBeanPostProcessor.kt
+++ src/main/java/sk/stuba/pks/starter/InjectMessageListenersAnnotationBeanPostProcessor.kt
@@ -1,5 +1,6 @@
 package sk.stuba.pks.starter
 
+import org.springframework.beans.factory.SmartInitializingSingleton
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.stereotype.Component
 import sk.stuba.pks.library.ConnectTo
@@ -8,8 +9,10 @@ import sk.stuba.pks.library.MessageListener
 
 
 @Component
-class InjectMessageListenersAnnotationBeanPostProcessor : BeanPostProcessor {
+class InjectMessageListenersAnnotationBeanPostProcessor : BeanPostProcessor, SmartInitializingSingleton {
     private val portToBeans = mutableMapOf<String, MutableList<MessageListener>>()
+    private val socketConnections = mutableListOf<SocketConnection>()
+
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
         if (bean is MessageListener) {
             val listenPortAnnotation = bean.javaClass.getAnnotation(ListenPort::class.java)
@@ -24,30 +27,31 @@ class InjectMessageListenersAnnotationBeanPostProcessor : BeanPostProcessor {
                 val listenersForPort = portToBeans.computeIfAbsent(port) { mutableListOf() }
                 listenersForPort.add(bean)
             }
-        }
-        if (bean is SocketConnection) {
-            try {
-                val socketListenerClass = bean::class.java
-                val portField = socketListenerClass.getDeclaredField("port")
-                portField.isAccessible = true
-                val port = portField[bean] as String
-
-                val listeners = portToBeans[port] ?: return bean
-                socketListenerClass.declaredFields.forEach { field ->
-                    field.annotations.forEach { annotation ->
-                        if (annotation is InjectMassageListeners) {
-                            field.isAccessible = true
-                            field[bean] = listeners
-                        }
-                    }
-                }
-            } catch (e: NoSuchFieldException) {
-                e.printStackTrace()
-            } catch (e: IllegalAccessException) {
-                e.printStackTrace()
-            }
+        } else if (bean is SocketConnection) {
+            socketConnections.add(bean)
         }
         return bean
     }
 
+    override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+        return bean
+    }
+
+    override fun afterSingletonsInstantiated() {
+        socketConnections.forEach { socketConnection ->
+            val socketListenerClass = socketConnection::class.java
+            val portField = socketListenerClass.getDeclaredField("port")
+            portField.isAccessible = true
+            val port = portField[socketConnection] as String
+            val listeners = portToBeans[port] ?: return@forEach
+            socketListenerClass.declaredFields.forEach { field ->
+                field.annotations.forEach { annotation ->
+                    if (annotation is InjectMassageListeners) {
+                        field.isAccessible = true
+                        field[socketConnection] = listeners
+                    }
+                }
+            }
+        }
+    }
 }
